5411f9792211e5a3e0f39bd2294ff41378c47ffa
wataru-g <grrenstar284@gmail.com>
2024-03-11 23:02:04 +0900

    Seederファイルを編集

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -21,9 +21,12 @@ class TestSeeder extends Seeder
         //UserSeederによって挿入されたユーザーのIDを取得します
         $userId = DB::table('users')->where('email', 'test@example.com')->value('id');
 
+        // UserSeederの変更に合わせて新しいユーザーIDを取得します
+        $newUserId = DB::table('users')->max('id') + 1;
+
         DB::table('tasks')->insert([
             [
-                'user_id' => 1,
+                'user_id' => $newUserId,
                 'title' => 'スポーツ',
                 'description' => 'テニスは楽しい',
                 'created_at' => now(),
@@ -31,7 +34,7 @@ class TestSeeder extends Seeder
             ],
 
             [
-                'user_id' => 1,
+                'user_id' => $newUserId,
                 'title' => '読書',
                 'description' => '参考書を買いに行きたい',
                 'created_at' => now(),
@@ -39,7 +42,7 @@ class TestSeeder extends Seeder
             ],
 
             [
-                'user_id' => 1,
+                'user_id' => $newUserId,
                 'title' => '買い物',
                 'description' => '人参、白菜、大根を買ってきて',
                 'created_at' => now(),
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,9 +17,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // 最大のユーザーIDを取得
+        $maxUserId = DB::table('users')->max('id');
+
+        // 新しいユーザーのIDを設定
+        $newUserId = $maxUserId + 1;
+
+        //ユーザーを挿入
         DB::table('users')->insert([
             [
-                'id' => 1,
+                'id' => $newUserId,
                 'name' => 'aaa',
                 'email' => 'test@example.com',
                 'password' => Hash::make('password123')
